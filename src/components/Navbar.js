a48d6f71dcd860fbd94b54d609ca7e0854b8e68d
ArduousEndeavour <mohitneg377@gmail.com>
2022-12-09 18:06:17 +0530

    Styled the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
-    <div className='Navbar'>
+    <div className='navbar'>
         <div className='container'>
-            <nav>
-                <h3>
-                    <Link to='/'><i className='fas fa-video'></i>Search</Link> 
+            <nav className='navbar__nav'>
+                <h3 className='nav__brand'>
+                    <Link to='/'><i className='fas fa-video'></i>&emsp;TV Maze</Link> 
                 </h3>
-                <ul>
-                    <li>
+                <ul className='nav__links'>
+                    <li className='links__link'>
                         <Link to="/">Home</Link>
                     </li>
-                    <li>
+                    <li className='links__link'>
                         <Link to="/about">About</Link>
                     </li>
                 </ul>
---
+++ src/components/Searchbar.js
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+export const Searchbar = () => {
+    const [searchTerm,setSearchTerm] = useState("");
+    const onSearchHandler = (e) =>{
+        e.preventDefault();
+        console.log("Searching for "+searchTerm);
+    }
+  return (
+    <div className='searchbar'>
+    <form className='searchbar_form'>
+        <input type="text" placeholder="Search for the Tv show"
+        value={searchTerm}
+        onChange={(e)=>{setSearchTerm(e.target.value)}}
+        />
+        <button className="btn btn-block" onClick={(onSearchHandler)}>Search</button>
+    </form>
+    </div>
+  )
+}
