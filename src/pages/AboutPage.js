652dc1a586b0abfb3362c8c55567a4c570bbae9e
ArduousEndeavour <mohitneg377@gmail.com>
2022-12-10 03:11:05 +0530

    Added about page with styling

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,5 +1,25 @@
 export const AboutPage = () => {
   return (
-    <div className='aboutpage'>AboutPage</div>
-  )
-}
+    <div className="about">
+      <h3 className="about__title">ABOUT PAGE</h3>
+      <div className="about__info">
+
+        <h3>ReactJS Developer Profile Set-2</h3>
+        <br/>
+        <div>
+          <p>From the given api link : https://api.tvmaze.com/search/shows?q=all</p>
+          <br/>
+          <h4>Skills used to build the app :</h4>
+          <ul>
+            <li>React (create-react-app)</li>
+            <li>Basic CSS & HTML</li>
+            <li>Axios AJAX Request</li>
+            <li>React Router</li>
+            <li>React Context API</li>
+            <li>Deploying to Netlify</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
