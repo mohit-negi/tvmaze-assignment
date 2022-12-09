4cfb0efc0b47ca546687ad1ff7ea1816e84a397e
ArduousEndeavour <mohitneg377@gmail.com>
2022-12-09 23:06:20 +0530

    Destructured shows and mapped in searchbar

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,23 @@
+import { useContext } from "react"
+//context
+import ShowsContext from "../context/shows/ShowsContext"
+//components
 import { Searchbar } from "../components/Searchbar"
 
 export const Homepage = () => {
+  const showsContext = useContext(ShowsContext);
+  const {loading,shows} = showsContext;
   return (
     <div className="homepage">
       <Searchbar/>
+      {
+      loading ? <h2>Loading.....</h2> :
+      <div>
+          {shows.map(item=>{
+            return <h3>{item.show.name}</h3>
+          })}
+        </div>
+      }
     </div>
   )
 }
